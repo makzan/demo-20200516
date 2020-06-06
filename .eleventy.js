8691700a1ef2c8ba1e2f64512b9d01bba397d25e
Thomas Seng Hin Mak <mak@makzan.net>
2020-06-06 15:04:26 +0800

    Add passthrough-copy to .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,13 @@
 module.exports = function(eleventyConfig) {
+  
+  // Folders to copy into output.
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("css");
+  
+  // If we use Netlify and has the _redirects file.
+  eleventyConfig.addPassthroughCopy("_redirects");
+
+  
 
   eleventyConfig.addFilter("toISOString", function(date) {
     return date.toISOString().split('T')[0];
